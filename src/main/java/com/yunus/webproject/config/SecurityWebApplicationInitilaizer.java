63ce3fa06bc43326ac347380f3e6b91144601377
Yunus Bağrıyanık <45238344+yunusbgrynk@users.noreply.github.com>
2019-03-27 04:37:02 +0300

    Update SecurityWebApplicationInitilaizer.java

--- src/main/java/com/yunus/webproject/config/SecurityWebApplicationInitilaizer.java
+++ src/main/java/com/yunus/webproject/config/SecurityWebApplicationInitilaizer.java
@@ -4,9 +4,6 @@ import org.springframework.security.web.context.AbstractSecurityWebApplicationIn
 
 public class SecurityWebApplicationInitilaizer extends AbstractSecurityWebApplicationInitializer {
 	
-	/*
-	 * The SpringSecurityInitializer class is the class that takes over the automatic upload task of SpringSecurityFilterChain. 
-	 * This makes the automatic installation process extend through the AbstractSecurityWebApplicationInitializer.
-	 */
+	
 
 }
